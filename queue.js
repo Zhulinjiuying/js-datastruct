bfa549133ecf04c66883205b248deb0138a1816c
Zhulinjiuying <554851075@qq.com>
2020-12-11 17:15:38 +0800

    new: queue

---
+++ queue.js
@@ -0,0 +1,61 @@
+'use strict';
+
+class Queue {
+  constructor() {
+    this._queue = {};
+    this._length = 0;
+    this._headIndex = 0;
+    this._endIndex = 0;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(value) {}
+
+  get queue() {
+    return this._queue;
+  }
+
+  set queue(value) {}
+
+  get headIndex() {
+    return this._headIndex;
+  }
+
+  set headIndex(value) {}
+
+  get endIndex() {
+    return this._endIndex;
+  }
+
+  set endIndex(value) {}
+
+  enQueue(item) {
+    this._endIndex = this._length;
+    this._queue[this._length++] = item;
+  }
+
+  deQueue() {
+    if (this.isEmpty()) {
+      return null;
+    } else {
+      const item = this._queue[this._headIndex];
+      this._queue[this._headIndex] = undefined;
+      this._headIndex++;
+      this._length--;
+      return item;
+    }
+  }
+
+  isEmpty() {
+    return this._length === 0;
+  }
+
+  size() {
+    return this._length;
+  }
+}
+
+module.exports = Queue;
